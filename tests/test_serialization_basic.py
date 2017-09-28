2dcf047e720e4e53d4972363473ff6e51ad43584
Bart6114 <bartsmeets86@gmail.com>
2017-09-28 07:31:39 +0200

    test serialization utility funcs

--- tests/test_serialization_basic.py
+++ tests/test_serialization_basic.py
@@ -45,7 +45,11 @@ def test_load_after_dump(resource_tmp_file):
     assert p(2) == 4
 
 
+def test_load_save_utility_funcs(resource_tmp_file):
+    from mlpipe.utils import save, load
+    p = Pipe() +\
+        Segment(lambda x: x+1, "step1") + \
+        Segment(lambda x: x+1, "step2")
 
-
-
-
+    save(p, resource_tmp_file)
+    load(resource_tmp_file)
