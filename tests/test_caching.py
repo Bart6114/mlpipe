c0e51cac17cda4490143c6770994bcd3cd2fd1f6
Bart6114 <bartsmeets86@gmail.com>
2017-10-03 10:38:04 +0200

    ignore cache test on py2

--- tests/test_caching.py
+++ tests/test_caching.py
@@ -1,10 +1,14 @@
 from mlpipe import Pipe
+import sys
+import pytest
 
+@pytest.mark.filterwarnings('ignore:Ignoring')
 def test_cache_basic():
-    p = Pipe(cached=True) +\
-        (lambda x: x)
+    if sys.version_info > (3, 0, 0):
+        p = Pipe(cached=True) +\
+            (lambda x: x)
 
-    assert p(2) == 2
-    assert p(2) == 2
-    assert p.__cache_info__()[1] == 1 # misses
-    assert p.__cache_info__()[0] == 1 # hits
+        assert p(2) == 2
+        assert p(2) == 2
+        assert p.__cache_info__()[1] == 1 # misses
+        assert p.__cache_info__()[0] == 1 # hits
